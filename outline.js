a730f230c9183b61666432b4f07495b48ad5486a
MsMishi <mcwcharles@gmail.com>
2017-05-08 14:58:30 -0700

    Game Rules

---
+++ outline.js
@@ -0,0 +1,36 @@
+DealerAi() {
+  if ((card1 + card2(visible to player)) <= 15) {
+    draw another card and add to sum
+  } else {
+    do nothing
+  }
+}
+
+hand() {
+  sum of cards
+}
+
+card
+case 10 || "J" || "Q" || "K":
+  {
+    hand + 10
+  }
+
+
+  ace() {
+    if (hand >= 21) {
+      "A" = 11
+    } else {
+      "A" = 1
+    }
+  }
+
+
+  Shuffle() {
+    shuffles deck
+  }
+
+  bankroll() {
+    applies to player
+    gameover <= 0
+  }

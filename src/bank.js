d1df68d53ecdeaa61f53bf6c72f5753b71f12c07
MsMishi <mcwcharles@gmail.com>
2017-05-11 14:23:41 -0700

    add bank

--- src/bank.js
+++ src/bank.js
@@ -1,7 +1,6 @@
-class Bank {
-constructor(account = 1000, score, bet = 10){
-  this.account = account,
-  this.score = score,
-  this.bet = bet
-  }
+function bank() {
+account = 1000
+wager = account - bet
+
+prompt("How much would you like to bet")
 }

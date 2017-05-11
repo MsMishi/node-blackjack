5345c4b9ac9944f216d5187471fd2d4bfb4aaed5
MsMishi <mcwcharles@gmail.com>
2017-05-10 20:41:58 -0700

    Update deck function

---
+++ src/deck.js
@@ -0,0 +1,21 @@
+class Deck {
+  constructor () {
+    this.cardDeck = []
+  }
+}
+function createDeck (){
+  const heart = ('♥'.red).bgWhite
+  const spade = ('♠'.black).bgWhite
+  const diamond = ('♦'.red).bgWhite
+  const club = ('♣'.black).bgWhite
+
+  const suits = [heart, spade, diamond, club]
+  const ranks = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']
+
+
+suits.forEach(function(suit){
+  ranks.forEach(function(rank){
+     cardDeck.push(new Card(suit, rank))
+    })
+  })
+}

c881e088a5d6a320b67c750539ee41febc82797d
MsMishi <mcwcharles@gmail.com>
2017-05-10 18:19:12 -0700

    Add src with card and deck file

---
+++ src/card.js
@@ -0,0 +1,15 @@
+class Card {
+  constructor(rank, suit) {
+  this.rank = rank,
+  this.suit = suit,
+  this.value = null;
+  }
+
+  cardValue(rank) {
+    if (isNaN(this.rank)){
+      this.value = 10
+    } else {
+      this.rank === this.value
+    }
+  }
+}

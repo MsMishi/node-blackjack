4dc2033707c09702c4d484d082292c7b471ff10f
MsMishi <mcwcharles@gmail.com>
2017-05-12 09:38:05 -0700

    Update branch

--- src/card.js
+++ src/card.js
@@ -1,17 +1,17 @@
-module.exports = Card
 class Card {
   constructor(rank, suit) {
-  this.rank = rank,
-  this.suit = suit,
-  this.value = null;
+    this.rank = rank,
+      this.suit = suit,
+      this.value = null;
   }
 
   cardValue(rank) {
-    if (isNaN(this.rank)){
+    if (isNaN(this.rank)) {
       this.value = 10
     } else {
       this.rank === this.value
     }
   }
- return new Card().toString()
 }
+
+module.export = Card
--- src/deck.js
+++ src/deck.js
@@ -1,32 +1,24 @@
-const _ = require('lodash')
-const col = require('colors')
-const card = require('/.card.js')
-
-module.exports = Deck
 class Deck {
-  constructor () {
+  constructor() {
     this.cardDeck = []
   }
+}
 
- createDeck (){
+function createDeck() {
   const heart = ('♥'.red).bgWhite
   const spade = ('♠'.black).bgWhite
   const diamond = ('♦'.red).bgWhite
   const club = ('♣'.black).bgWhite
 
   const suits = [heart, spade, diamond, club]
-  const ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J','Q','K']
+  const ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 
 
-suits.forEach(function(suit){
-  ranks.forEach(function(rank){
-     this.cardDeck.push(new Card(suit, rank))
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      cardDeck.push(new Card(suit, rank))
     })
   })
 }
 
-
-
-// shuffle(){
-//   this.cardDeck = _.shuffle(cardDeck)
-// }
+module.exports = Deck

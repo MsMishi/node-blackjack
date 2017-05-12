2b50fa5ee66f6f96993737d821f671ae5def1664
MsMishi <mcwcharles@gmail.com>
2017-05-12 09:48:40 -0700

    Update Bank

--- src/bank.js
+++ src/bank.js
@@ -3,7 +3,7 @@ module.export = Bank
 class Bank{
   constructor(account, bet, wager){
 this.account = this.account - this.bet || 100,
-this.bet = prompt("You have " + this.account + ". Place your bet")
+this.bet = bet,
 
   if (bet <= 0){
     prompt("You have " + this.account + ". Place your bet")
